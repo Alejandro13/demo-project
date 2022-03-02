dc4a32be3c150201b8dbe6533351e0714faa7765
Alejandro Martínez <lexmart13@gmail.com>
2022-03-02 15:28:31 -0600

    step2: update controller

--- src/main/java/com/demo/steps/controller/Tasks1Controller.java
+++ src/main/java/com/demo/steps/controller/Tasks1Controller.java
@@ -1,5 +1,7 @@
 package com.demo.steps.controller;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.demo.steps.entities.Task;
@@ -8,41 +10,85 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //da la capacidad de exponer el api por medio de http
-@RequestMapping("/tasks") //Para modificar la ruta de acceso -> /tasks/all
+@RequestMapping("/tasks") //Para modificar la ruta de acceso -> /tasks/accion
 public class Tasks1Controller {
+
+	private final List<Task> taskList = new ArrayList<>(); 
+
     //Endpoint
-	//Despues del 8080 va el /hi
-	
-	@GetMapping("/all")
+	//Despues del 8080 va el /	
+	@GetMapping("") //Al invocar el route tasks con get nos devolvera la lista 
 	public ResponseEntity<List< Task> > fetchAllTask(){
 		//return "[ returning all tasks ";
-       //return ResponseEntity.ok().body("ALL TASKS");
-        return null;
+       	return ResponseEntity.ok().body(taskList); //devolvemos una lista de tareas
+        
 	}
 
-	@PostMapping("/save")
-	public ResponseEntity<Task> postTask( Task newTask){
+	@PostMapping("")//Al invocar el route tasks con post guardara la tarea en la lista 
+	public ResponseEntity<Task> postTask( @RequestBody Task newTask){ //Lo que venga en el requestBody lo va transformar en un objeto java
+		newTask.setActive(false);
+		newTask.setCreatedAt( LocalDateTime.now() ); //Seteamos la fecha
+		
+		long id =  taskList.size() + 1 ;
+		newTask.setId(id);
+
+		taskList.add(newTask); //agregamos la tarea a la lista
+		
 		//return "saving..";
-        //return ResponseEntity.status(HttpStatus.CREATED).body("task created");
-        return null;
+		//Devolvemos un created con el nuevo objeto creado en el body
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTask);
+        
 	}
 
-	@DeleteMapping("/delete")
-	public ResponseEntity<Void> deleteTask( Long taskId){
+	@GetMapping("/{taskId}") ///{variable}
+	public ResponseEntity<Task> fetchTaskById( @PathVariable("taskId") Long taskId ){
+		return	taskList
+				.stream()
+				.filter( current -> taskId == current.getId() ) //es la condicion, te devuelve el array que encuentre
+				.findFirst() // te trae el primer elemento que encuentre
+				.map( task -> ResponseEntity.ok().body(task)) //si ese elemento es != de null, se regresa el task y un cogido ok
+				.orElse( ResponseEntity.status(HttpStatus.NOT_FOUND ).build() ); //Si es null
+	}
+
+
+	@DeleteMapping("/{taskId}")
+	public ResponseEntity<?> deleteTask( @PathVariable("taskId") Long taskId){
+		return taskList
+						.stream() //
+						.filter( current -> taskId == current.getId() )
+						.findFirst()
+						.map( task -> {
+							taskList.remove( task);//borra el elemento de la lista
+							return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+						}) //para varias opciones
+						.orElse( ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 		//return "delete...";
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        //return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 
-	@PutMapping("/update")
-	public ResponseEntity<String> putTask( Long taskId, Task updateTask){
+	@PutMapping("/{taskId}")
+	public ResponseEntity<Task> putTask( @PathVariable("taskId") Long taskId, @RequestBody Task updateTask){
 		//return " update";
-        return ResponseEntity.ok().body("update");
+		return taskList
+						.stream() //
+						.filter( current -> taskId == current.getId() )
+						.findFirst()
+						.map( task -> {
+							task.setTitle( updateTask.getTitle());
+							task.setDescription( updateTask.getDescription() );
+							task.setUpdateAt( LocalDateTime.now() );
+							return ResponseEntity.ok().body(task); //devuelve la tarea actualizada
+						})
+						.orElse( ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
     
 }
--- src/main/java/com/demo/steps/entities/Task.java
+++ src/main/java/com/demo/steps/entities/Task.java
@@ -1,10 +1,16 @@
 package com.demo.steps.entities;
+
+import java.time.LocalDateTime;
+
 //Esta clase sera nuestro modelo de la entidad
 public class Task {
 
     private Long id;
     private String title;
     private String description;
+    private Boolean active;
+    private LocalDateTime createdAt;
+    private LocalDateTime updateAt;
 
     //Getter & setter
 
@@ -26,6 +32,26 @@ public class Task {
     public void setDescription(String description) {
         this.description = description;
     }
+    public Boolean getActive() {
+        return this.active;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public LocalDateTime getUpdateAt() {
+        return this.updateAt;
+    }
+    public void setUpdateAt(LocalDateTime updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    
 
     
 }
